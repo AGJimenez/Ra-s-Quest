ac99a2ff6ee46cfc9e18c34b9a708e68de2f813c
Alex Gámez <120292381+AGJimenez@users.noreply.github.com>
2024-04-22 15:59:19 +0200

    diseño de nivel

---
+++ nivelAle/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var speed = 100
+var player_state
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.length() == 0:
+		player_state = "idle"
+	else:
+		player_state = "walking"
+	
+	velocity = direction.normalized() * speed
+	move_and_slide()
+	playAnim(direction)
+
+func playAnim(dir):
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	elif player_state == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("walk-b")
+		elif dir.x == 1:
+			$AnimatedSprite2D.play("walk-r")
+		elif dir.y == 1:
+			$AnimatedSprite2D.play("walk-f")
+		elif dir.x == -1:
+			$AnimatedSprite2D.play("walk-l")
