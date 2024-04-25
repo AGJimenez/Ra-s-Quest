0d7bc84bddfde95bfb26eeaab31741fa44da87a3
DanielPerezMorales <131249984+DanielPerezMorales@users.noreply.github.com>
2024-04-25 23:19:37 +0200

    Arreglo de la mierda de personaje de mario

--- Nivel Dani/Script/personaje.gd
+++ Nivel Dani/Script/personaje.gd
@@ -27,11 +27,7 @@ func _physics_process(delta):
 
 	velocity = direccion * speed
 	
-	if on_area_cofre ==true and Input.is_action_just_pressed("Interactuar"):
-		interaccion_con_cofre()
-	elif on_area_balanza==true and Input.is_action_just_pressed("Interactuar"):
-		interaccion_con_balanza()
-
+	interaccion()
 	update_animation()
 	move_and_slide()
 
@@ -50,100 +46,112 @@ func interaccion_con_cofre():
 	else:
 		panel_visible.visible = true
 		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/panel_respuesta.dialogue"),"main")
-		
 
+func interaccion():
+	if on_area_cofre ==true and Input.is_action_just_pressed("Interactuar"):
+		print("DALE A LA E")
+		interaccion_con_cofre()
+	elif on_area_balanza==true and Input.is_action_just_pressed("Interactuar"):
+		interaccion_con_balanza()
 
 func interaccion_con_balanza():
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/interaccion_con_balanza.dialogue"),"main")
 
 
+
 func _on_area_cofres_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_2_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_2_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_3_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_3_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_4_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_4_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_5_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_5_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_6_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_6_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_balanza_maat_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_balanza=true
 func _on_balanza_maat_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_balanza=false
 func _on_area_cofres_vertical_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_2_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_2_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_3_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_3_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_4_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_4_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_5_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_5_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_6_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_6_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_7_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_7_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
 func _on_area_cofres_vertical_8_body_entered(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
 		on_area_cofre=true
 func _on_area_cofres_vertical_8_body_exited(body):
-	if body.name=="Personaje":
+	if body.name=="CharacterBody2D":
+		on_area_cofre=false
+func _on_area_cofres_vertical_9_body_entered(body):
+	if body.name=="CharacterBody2D":
+		on_area_cofre=true
+func _on_area_cofres_vertical_9_body_exited(body):
+	if body.name=="CharacterBody2D":
 		on_area_cofre=false
