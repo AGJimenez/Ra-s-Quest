4a98187fe19123a3bfb2a7e7cd2ac9c3b2abdadc
DanielPerezMorales <131249984+DanielPerezMorales@users.noreply.github.com>
2024-04-29 19:49:27 +0200

    Y sorted arreglao (sus muertos)

--- Nivel Dani/Script/node_2d.gd
+++ Nivel Dani/Script/node_2d.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Conversacion_Principal.dialogue"),"main")
-	return 
+	var nueva_posicion = Vector2(88, 0) # Nueva posición deseada
+	self.position = nueva_posicion
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
