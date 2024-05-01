e69bbf791b671ffd2220a75a8f18eadee14ba188
DanielPerezMorales <131249984+DanielPerezMorales@users.noreply.github.com>
2024-05-01 20:48:08 +0200

    Funcionalidad quitada del personaje

--- Nivel Dani/Script/node_2d.gd
+++ Nivel Dani/Script/node_2d.gd
@@ -1,11 +1,204 @@
 extends Node2D
 
-
+var on_area_cofre=false
+var on_area_balanza=false
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Conversacion_Principal.dialogue"),"main")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	interaccion()
 
+
+func interaccion_con_cofre():
+	var panel_visible : Panel = $CharacterBody2D/Camera2D/Panel
+	if panel_visible.visible:
+		panel_visible.visible = false
+	else:
+		panel_visible.visible = true
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/panel_respuesta.dialogue"),"main")
+
+func interaccion():
+	if on_area_cofre ==true and Input.is_action_just_pressed("Interactuar"):
+		print("DALE A LA E")
+		interaccion_con_cofre()
+	elif on_area_balanza==true and Input.is_action_just_pressed("Interactuar"):
+		interaccion_con_balanza()
+
+func interaccion_con_balanza():
+	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/interaccion_con_balanza.dialogue"),"main")
+
+
+func _on_area_cofres_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_2_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres2
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_2_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_3_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres3
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_3_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_4_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres4
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_4_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_2_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical2
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_2_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_3_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical3
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_3_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_4_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical4
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_4_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_5_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical5
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_5_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_6_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical6
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_6_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_7_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical7
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_7_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_8_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical8
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_8_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_area_cofres_vertical_9_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=true
+		$CharacterBody2D/Camera2D/Panel.ultima_Area=$Cofres/area_cofres_vertical9
+		print("TUS MUERTOS")
+
+
+func _on_area_cofres_vertical_9_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_balanza_maat_body_entered(body):
+	if(body.is_in_group("Personaje")):
+		on_area_balanza=true
+
+
+func _on_balanza_maat_body_exited(body):
+	if(body.is_in_group("Personaje")):
+		on_area_cofre=false
+
+
+func _on_introduccion_area_body_entered(body):
+	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/interaccion_con_Anubis.dialogue"),"main")
+
+
+func _on_introduccion_area_body_exited(body):
+	$introduccion_area.queue_free()
--- Nivel Dani/Script/personaje.gd
+++ Nivel Dani/Script/personaje.gd
@@ -4,8 +4,6 @@ class_name Personaje
 var speed = 120
 var direccion = Vector2.ZERO
 var lastDirection = ""
-var on_area_cofre=false
-var on_area_balanza=false
 @onready var anim = $AnimatedSprite2D
 
 # MÉTODO ASIGNACIÓN DIRECCIONES
@@ -26,8 +24,6 @@ func _physics_process(delta):
 		lastDirection = "Down"
 
 	velocity = direccion * speed
-	
-	interaccion()
 	update_animation()
 	move_and_slide()
 
@@ -38,137 +34,3 @@ func update_animation():
 	else:
 		anim.play("stop" + lastDirection)
 
-
-func interaccion_con_cofre():
-	var panel_visible : Panel = $Camera2D/Panel
-	if panel_visible.visible:
-		panel_visible.visible = false
-	else:
-		panel_visible.visible = true
-		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/panel_respuesta.dialogue"),"main")
-
-func interaccion():
-	if on_area_cofre ==true and Input.is_action_just_pressed("Interactuar"):
-		print("DALE A LA E")
-		interaccion_con_cofre()
-	elif on_area_balanza==true and Input.is_action_just_pressed("Interactuar"):
-		interaccion_con_balanza()
-
-func interaccion_con_balanza():
-	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/interaccion_con_balanza.dialogue"),"main")
-
-
-func _on_area_cofres_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres"
-		print("TUS MUERTOS")
-func _on_area_cofres_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_2_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres2"
-func _on_area_cofres_2_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_3_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres3"
-func _on_area_cofres_3_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_4_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_4_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres4"
-func _on_area_cofres_vertical_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical"
-func _on_area_cofres_vertical_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_2_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical2"
-func _on_area_cofres_vertical_2_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_3_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical3"
-func _on_area_cofres_vertical_3_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_4_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical4"
-func _on_area_cofres_vertical_4_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_5_body_entered(body):
-	if body.name=="CharacterBody2D":
-		print("TUS MUERTOS")
-		on_area_cofre=true
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical5"
-func _on_area_cofres_vertical_5_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_6_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical6"
-func _on_area_cofres_vertical_6_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_7_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical7"
-func _on_area_cofres_vertical_7_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_8_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical8"
-func _on_area_cofres_vertical_8_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_area_cofres_vertical_9_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=true
-		print("TUS MUERTOS")
-		$Camera2D/Panel.ultima_Area=$"../Cofres/area_cofres_vertical9"
-func _on_area_cofres_vertical_9_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_cofre=false
-func _on_balanza_maat_body_entered(body):
-	if body.name=="CharacterBody2D":
-		on_area_balanza=true
-func _on_balanza_maat_body_exited(body):
-	if body.name=="CharacterBody2D":
-		on_area_balanza=false
-func _on_introduccion_area_body_entered(body):
-	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/interaccion_con_Anubis.dialogue"),"main")
-func _on_introduccion_area_body_exited(body):
-	$"../introduccion_area".queue_free()
