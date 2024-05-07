2edb8c3f942810eb079de34d065bab71ae7b83f3
DanielPerezMorales <131249984+DanielPerezMorales@users.noreply.github.com>
2024-05-07 20:11:26 +0200

    Ecuacion 3er grado y todo metido en mi nivel

--- Global/Scripts/node_2d.gd
+++ Global/Scripts/node_2d.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var on_area_cofre=false
 var on_area_balanza=false
-@onready var panel = get_node("Personaje/CharacterBody2D/Camera2D/Panel_Dani")
+@onready var panel = get_node("Personaje/Camera2D/Panel")
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Dani/Conversacion_Principal.dialogue"),"main")
@@ -31,168 +31,168 @@ func interaccion_con_balanza():
 
 
 func _on_area_cofres_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_2_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres2
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_2_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_3_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres3
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_3_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_4_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres4
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_4_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_2_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical2
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_2_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_3_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area = $Cofres/area_cofres_vertical3
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_3_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_4_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical4
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_4_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_5_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical5
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_5_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_6_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical6
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_6_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_7_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical7
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_7_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_8_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical8
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_8_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_area_cofres_vertical_9_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=true
 		panel.ultima_Area=$Cofres/area_cofres_vertical9
 		print("TUS MUERTOS")
 
 
 func _on_area_cofres_vertical_9_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
 func _on_balanza_maat_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_balanza=true
 
 
 func _on_balanza_maat_body_exited(body):
-	if body.name == "CharacterBody2D":
+	if(body.is_in_group("Personaje")):
 		on_area_cofre=false
 
 
