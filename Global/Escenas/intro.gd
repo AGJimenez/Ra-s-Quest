f96e9542027060f6a672fd948d286c19ac4e84e6
Alex Gámez <120292381+AGJimenez@users.noreply.github.com>
2024-05-07 20:03:34 +0200

    Intro con camara

--- Global/Escenas/intro.gd
+++ Global/Escenas/intro.gd
@@ -4,8 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("new_animation")
-
-
+	$Personaje/Camera2D.enabled = false
+	$AnimationPlayer2.play("camara")
+	await get_tree().create_timer(5).timeout
+	$Camera2D.enabled = false
+	$Personaje/Camera2D.enabled = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
