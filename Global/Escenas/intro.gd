86596640fb018a3a847defc089ddd7408dbaa442
Alex Gámez <120292381+AGJimenez@users.noreply.github.com>
2024-05-08 19:40:55 +0200

    Dialoguitosss

--- Global/Escenas/intro.gd
+++ Global/Escenas/intro.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var enAreaNPC = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$npc/Control.visible = false
 	$AnimationPlayer.play("new_animation")
 	$Personaje/Camera2D.enabled = false
 	$AnimationPlayer2.play("camara")
@@ -11,8 +13,8 @@ func _ready():
 	$Personaje/Camera2D.enabled = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if enAreaNPC == true and Input.is_action_just_pressed("Interact"):
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/intro/controlesNpc.dialogue"),"this_is_a_node_title")
 
 
 func _on_area_2d_body_entered(body):
@@ -22,4 +24,12 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2dnpc_body_entered(body):
 	if body.name=="Personaje":
+		enAreaNPC = true
+		$npc/Control.visible = true
 		print("Conversacion con npc")
+
+
+
+func _on_area_2dnpc_body_exited(body):
+	enAreaNPC = false
+	$npc/Control.visible = false
--- Global/Personaje/npcIntro.gd
+++ Global/Personaje/npcIntro.gd
@@ -2,4 +2,4 @@ extends CharacterBody2D
 
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.play("idle_left")
