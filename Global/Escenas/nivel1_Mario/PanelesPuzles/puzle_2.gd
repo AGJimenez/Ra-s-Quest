b7b96c0e0d6a84e55b433640f17ed0ba7b94b869
MarioGarcia8 <mariogarlop03@gmail.com>
2024-05-14 22:37:19 +0200

    puzle 2 va mejorando, mñn mas sufrimiento

--- Global/Escenas/nivel1_Mario/PanelesPuzles/puzle_2.gd
+++ Global/Escenas/nivel1_Mario/PanelesPuzles/puzle_2.gd
@@ -14,14 +14,13 @@ func _ready():
 
 func _process(_delta):
 	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouse):
-		print(mouse)
 		var mouse_copy = mouse
+		print(mouse.position)
 		mouse = false
-		var rows = int(mouse_copy.y / 80)
-		var cols = int(mouse_copy.x / 80) 
-		if (cols < 0):
-			cols = 0
-		print(rows, cols)
+		var rows = int(mouse_copy.position.y / 128 ) - 1
+		var cols = int(mouse_copy.position.x / 128 ) - 3
+		print(rows, ",", cols)
+		print(piezas[rows*3+cols])
 		check_blank_space(rows, cols)
 		if piezas == resuelto:
 			print("HAS GANADO")
@@ -40,13 +39,12 @@ func shuffle_tiles():
 	for t in range(0, 1000):
 		var tile = randi() % 9
 		if (piezas[tile] != $Pieza7 and tile != previous and tile != previous_1):
-			var rows = int(piezas[tile].position.y / 80)
-			var cols = int(piezas[tile].position.x / 80)
-			if (cols < 0):
-				cols = 0
+			var rows = int(piezas[tile].position.y / 128)
+			var cols = int(piezas[tile].position.x / 128)
 			check_blank_space(rows, cols)
 			previous_1 = previous
 			previous = tile
+
 
 
 func check_blank_space(rows, cols):
@@ -56,27 +54,28 @@ func check_blank_space(rows, cols):
 	while (!empty and !done):
 		var new_pos = piezas[pos].position
 		if (rows < 2):
-			new_pos.y += 80
+			new_pos.y += 128
 			empty = find_empty(new_pos, pos)
-			new_pos.y -= 80
-		if (rows >0):
-			new_pos.y -= 80
+			new_pos.y -= 128
+		if (rows > 0):
+			new_pos.y -= 128
 			empty = find_empty(new_pos, pos)
-			new_pos.y += 80
+			new_pos.y += 128
 		if (cols < 2):
-			new_pos.x += 80
+			new_pos.x += 128
 			empty = find_empty(new_pos, pos)
-			new_pos.x -= 80
+			new_pos.x -= 128
 		if (cols > 0):
-			new_pos.x -= 80
+			new_pos.x -= 128
 			empty = find_empty(new_pos, pos)
-			new_pos.x += 80
+			new_pos.x += 128
 		done = true
 
 
+
 func find_empty(position, pos):
-	var new_rows = int(position.y / 80)
-	var new_cols = int(position.x / 80)
+	var new_rows = int(position.y / 128) -1
+	var new_cols = int(position.x / 128) -3
 	if (new_cols < 0):
 			new_cols = 0
 	var new_pos = new_rows * 3 + new_cols
@@ -85,6 +84,7 @@ func find_empty(position, pos):
 		return true
 	else:
 		return false
+
 
 
 func swap_tiles(tile_src, tile_dst):
@@ -99,13 +99,4 @@ func swap_tiles(tile_src, tile_dst):
 
 func _on_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and event.pressed):
-		#mouse = event
-		var global_mouse_pos = viewport.get_mouse_position()
-		var sprite_size = Vector2(176.3, 80)
-		for i in range(0, 9):
-			var pieza_rect = Rect2(piezas[i].position, sprite_size)
-			if pieza_rect.has_point(Vector2(global_mouse_pos.x, global_mouse_pos.y)):
-				print("Datos: ", global_mouse_pos, ", ", pieza_rect)
-				print("Posición de la pieza ", i, ": ", piezas[i].position)
-				mouse = piezas[i].position
-				break
+		mouse = event
