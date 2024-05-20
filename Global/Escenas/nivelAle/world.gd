34938f649f3c0a574865dab5909f5f742f432005
Sulongl <131246748+Sulongl@users.noreply.github.com>
2024-05-20 14:16:08 +0200

    pause in Ale lvl

--- Global/Escenas/nivelAle/world.gd
+++ Global/Escenas/nivelAle/world.gd
@@ -5,7 +5,6 @@ var finNivel = false
 var enAreaAltar = false
 var enAreaPlayer = false
 var enAreaPlayerCofre = false
-var paused = false
 var ignorarMov = false
 var transComplete = false
 var segundoPuzzleActivado = false
@@ -101,33 +100,6 @@ func _process(_delta):
 			$player.set_physics_process(false)
 		if(Global.dialogue_state == false):
 			$player.set_physics_process(true)
-		
-		
-	if Input.is_action_just_pressed("pausa"):
-		pauseMenu()
-		
-
-func pauseMenu():
-	if paused:
-		$player/Camera2D/CanvasLayer.hide()
-		Engine.time_scale = 1
-	else:
-		$player/Camera2D/CanvasLayer.show()
-		Engine.time_scale = 0
-	paused = !paused
-	ignorarMov = !ignorarMov
-	
-func resumeGame():
-	$player/Camera2D/CanvasLayer.hide()
-	Engine.time_scale = 1
-	ignorarMov = false
-	
-func reloadLevel():
-	get_tree().reload_current_scene()
-	resumeGame()
-
-func quitGame():
-	get_tree().quit()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "player":
@@ -139,17 +111,6 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(_body):
 	enAreaPlayer = false
 	$Area2D/Control.visible = false
-
-
-func _on_button_pressed():
-	resumeGame()
-
-func _on_button_2_pressed():
-	reloadLevel()
-
-
-func _on_button_3_pressed():
-	quitGame()
 
 
 func _on_area_2d_2_body_entered(body):
